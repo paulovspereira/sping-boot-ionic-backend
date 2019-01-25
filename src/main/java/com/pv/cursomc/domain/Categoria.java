57115e4fa806a7f1c6b25d76a522ad502319b816
Paulo Pereira <paulovspereira@gmail.com>
2019-01-25 00:12:12 -0200

    Banco de dados H2 criação de dados

--- src/main/java/com/pv/cursomc/domain/Categoria.java
+++ src/main/java/com/pv/cursomc/domain/Categoria.java
@@ -2,6 +2,13 @@ package com.pv.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class Categoria implements Serializable{
 
 	
@@ -10,6 +17,9 @@ public class Categoria implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
