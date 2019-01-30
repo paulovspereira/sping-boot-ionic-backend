3ade5b3c42325ceb7eee186a1595b447499dff8e
Paulo Pereira <paulovspereira@gmail.com>
2019-01-30 20:48:58 -0200

    Endpoint Cliente/id

--- src/main/java/com/pv/cursomc/CursomcApplication.java
+++ src/main/java/com/pv/cursomc/CursomcApplication.java
@@ -56,6 +56,7 @@ public class CursomcApplication implements CommandLineRunner{
 		Produto p2 = new Produto (null, "Impressora", 800.00);
 		Produto p3 = new Produto (null, "Mouse", 80.00);
 
+
 		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
 
---
+++ src/main/java/com/pv/cursomc/resources/ClienteResource.java
@@ -0,0 +1,29 @@
+package com.pv.cursomc.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pv.cursomc.domain.Cliente;
+import com.pv.cursomc.service.ClienteService;
+
+@RestController
+@RequestMapping(value="/clientes")
+public class ClienteResource {
+
+	@Autowired
+	private ClienteService servive;
+	
+	@RequestMapping(value="/{id}",method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		
+		Cliente obj  = servive.buscar(id);
+		return ResponseEntity.ok().body(obj);
+  }
+}
