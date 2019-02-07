a9e84afc19dfc015040b90db1e457b502dc67d36
Paulo Pereira <paulovspereira@gmail.com>
2019-02-07 12:36:18 -0200

    Inserindo um novo cliente para o post

--- src/main/java/com/pv/cursomc/service/ClienteService.java
+++ src/main/java/com/pv/cursomc/service/ClienteService.java
@@ -10,10 +10,17 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.pv.cursomc.domain.Categoria;
+import com.pv.cursomc.domain.Cidade;
 import com.pv.cursomc.domain.Cliente;
+import com.pv.cursomc.domain.Endereco;
+import com.pv.cursomc.domain.enuns.TipoCliente;
 import com.pv.cursomc.domain.Cliente;
 import com.pv.cursomc.dto.ClienteDTO;
+import com.pv.cursomc.dto.ClienteNewDTO;
+import com.pv.cursomc.repositories.CidadeRepository;
 import com.pv.cursomc.repositories.ClienteRepository;
+import com.pv.cursomc.repositories.EnderecoRepository;
 import com.pv.cursomc.service.exception.DataIntegrityExcepetion;
 import com.pv.cursomc.service.exception.ObjectNotFoundException;
 
@@ -23,12 +30,27 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				      "Objeto não encontrado! ID: " + id + ", Tipo: " + Cliente.class.getName()));
 		
 	}
+	
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+		
+	}
+	
 	
 	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId());
@@ -64,5 +86,22 @@ public class ClienteService {
 	
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Optional<Cidade> cid =  cidadeRepository.findById(objDto.getCidadeId());
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid.get());
+		cli.getEnderecos().add(end);
+		cli.getTelefone().add(objDto.getTelefone1());
+		
+		if(objDto.getTelefone2()!= null) {
+			cli.getTelefone().add(objDto.getTelefone2());
+		}
+		if(objDto.getTelefone3()!= null) {
+			cli.getTelefone().add(objDto.getTelefone2());
+		}
+		
+		return cli;
 	}
 }
