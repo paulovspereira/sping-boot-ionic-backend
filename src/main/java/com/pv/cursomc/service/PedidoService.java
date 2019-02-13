167fb4f8d455da105d81cf17817d95baa23e2bd5
Paulo Pereira <paulovspereira@gmail.com>
2019-02-13 17:38:50 -0200

    Email Html

--- src/main/java/com/pv/cursomc/service/PedidoService.java
+++ src/main/java/com/pv/cursomc/service/PedidoService.java
@@ -79,7 +79,9 @@ public class PedidoService {
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
+        
 		emailService.sendOrderConfirmationEmail(obj);
+		
 		return obj;
 	}
 }
--- src/main/java/com/pv/cursomc/service/SmtpEmailService.java
+++ src/main/java/com/pv/cursomc/service/SmtpEmailService.java
@@ -1,10 +1,13 @@
 package com.pv.cursomc.service;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 public class SmtpEmailService extends AbstractEmailService {
 
@@ -13,12 +16,22 @@ public class SmtpEmailService extends AbstractEmailService {
 	
     @Autowired
     private MailSender mailSender;
+    
+    @Autowired
+    private JavaMailSender JavaMailSender;
+    
 	@Override
 	public void sendEmail(SimpleMailMessage msg) {
 		 LOG.info("Envio de email....");
-	      LOG.info(msg.toString());
-	      LOG.info("Email enviado");
+         mailSender.send(msg);
+		 LOG.info("Email enviado");
 		
+	}
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Envio de email ....");
+		JavaMailSender.send(msg);	      
+		 LOG.info("Email enviado");		
 	}
 
 }
