115464f356149c540e6d8eabecfdbe3c760ce6f7
Paulo Pereira <paulovspereira@gmail.com>
2019-02-22 13:34:35 -0300

    Bonus Ajustando a Imagem

--- src/main/java/com/pv/cursomc/service/ClienteService.java
+++ src/main/java/com/pv/cursomc/service/ClienteService.java
@@ -141,6 +141,9 @@ public class ClienteService {
 		}
 		
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
+
 		String fileName = prefix + user.getId() + ".jpg";
 
 		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
